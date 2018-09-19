bf04763252ae6c42edf1351d5198a38b4cc64353
Lukasz <schwarzblade@gmail.com>
2018-09-19 20:00:50 +0200

    banners
    added_baners_some_html

---
+++ js/map.js
@@ -0,0 +1,16 @@
+
+function myMap() {
+  var myCenter = new google.maps.LatLng(49.674840,20.055930);
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {center: myCenter, zoom: 5};
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var marker = new google.maps.Marker({position:myCenter});
+  marker.setMap(map);
+
+  var infowindow = new google.maps.InfoWindow({
+    content: "Słoneczko Jak u Mamy!"
+  });
+  infowindow.open(map,marker);
+}
+
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD8LQPqDI8Xzg64ABVmSlTaVrjuOotSZ58&callback=myMap"
--- js/slide.js
+++ js/slide.js
@@ -1,19 +1,14 @@
 var slideIndex = 0;
-showSlides();
+carousel();
 
-function showSlides() {
+function carousel() {
     var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 2000); // Change image every 2 seconds
 }
